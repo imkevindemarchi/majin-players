317cb1260078767dfad4dc9e6b5cfcb49c2445f8
Kevin De Marchi <imkevindemarchi@gmail.com>
2024-10-31 09:53:36 +0100

    9 - code refactor

--- src/components/Button.component.jsx
+++ src/components/Button.component.jsx
@@ -4,11 +4,12 @@ const Button = ({ disabled, children }) => {
     return (
         <button
             disabled={disabled}
-            className={`w-full py-3 rounded-lg border-none outline-2 text-white ${
-                disabled
-                    ? "bg-gray-200 cursor-not-allowed outline-gray-200"
-                    : "bg-primary outline-primary hover:opacity-80  transition-all duration-200"
-            }`}
+            className={`w-full py-3 rounded-lg border-none outline-2 text-white 
+                ${
+                    disabled
+                        ? "bg-gray-200 cursor-not-allowed outline-gray-200"
+                        : "bg-primary outline-primary hover:opacity-80  transition-all duration-200"
+                }`}
         >
             {children}
         </button>
