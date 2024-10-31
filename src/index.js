72131307c00acbc1fd08d63af70ca37b01b11100
Kevin De Marchi <imkevindemarchi@gmail.com>
2024-10-31 09:50:05 +0100

    8 - code refactor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 
 // Assets
 import "./index.css";
 
 // Components
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
 
 const root = createRoot(document.getElementById("root"));
 
