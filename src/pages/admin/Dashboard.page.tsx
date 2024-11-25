4a7edce4da82f4762d29cc1d319ecce2df92a50e
Kevin De Marchi <imkevindemarchi@gmail.com>
2024-11-25 23:50:01 +0100

    43 - create the admin dashboard page

---
+++ src/pages/admin/Dashboard.page.tsx
@@ -0,0 +1,46 @@
+import { FC, useContext } from "react";
+
+// Assets
+import logoImg from "../../assets/images/logo.png";
+
+// Contexts
+import { ThemeContext } from "../../providers";
+
+// Types
+import { ThemeContextI } from "../../types";
+
+const Dashboard: FC = () => {
+    const { state: theme } = useContext(ThemeContext) as ThemeContextI;
+
+    const isDarkMode: boolean = theme === "dark";
+
+    const image = (
+        <img
+            src={logoImg}
+            className="w-[50vh] object-contain"
+            alt="Impossibile visualizzare l'immagine."
+        />
+    );
+
+    const title = (
+        <span
+            className={`absolute text-[3em] uppercase tracking-[1em] font-bold transition-all duration-200
+                ${isDarkMode ? "text-white" : "text-black"}
+            `}
+            style={{
+                textShadow: `0px 0px 5px ${isDarkMode ? "black" : "white"}`,
+            }}
+        >
+            {process.env.REACT_APP_WEBSITE_NAME}
+        </span>
+    );
+
+    return (
+        <div className="flex justify-center items-center relative">
+            {image}
+            {title}
+        </div>
+    );
+};
+
+export default Dashboard;
